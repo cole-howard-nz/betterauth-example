d4d752ca946f4d826a27c4a4f2de7db439a3bbb3
cole <osrunescape.colehoward@gmail.com>
2025-06-24 15:24:11 +1200

    added serverside authentication check to dashboard

--- betterauth/app/dashboard/page.tsx
+++ betterauth/app/dashboard/page.tsx
@@ -1,9 +1,21 @@
 import { SettingsDialog } from "@/components/settings-dialog"
+import { auth } from "@/lib/auth/auth"
+import { headers } from "next/headers"
 
-export default function Page() {
+const Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+
+  if (!session){
+    return <div>Not authenticated</div>
+  }
+
   return (
     <div className="flex h-svh items-center justify-center">
       <SettingsDialog />
     </div>
   )
 }
+
+export default Page
